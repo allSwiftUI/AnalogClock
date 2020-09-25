507e264c2ef51a684f5b26243fb0d5fc50c6d999
ASUI <asui@allswiftui.com>
2020-09-25 10:11:52 -0300

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------------
+// File: AnalogClockApp.swift
+// Package: AnalogClock
+//
+// Created by: ALLSWIFTUI on 25-09-20
+// Copyright © 2020 allSwiftUI · https://allswiftui.com · @allswiftui
+// -----------------------------------------------------------------------------
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnalogClock/ContentView.swift
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------------------
+// File: ContentView.swift
+// Package: AnalogClock
+//
+// Created by: ALLSWIFTUI on 25-09-20
+// Copyright © 2020 allSwiftUI · https://allswiftui.com · @allswiftui
+// -----------------------------------------------------------------------------
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
